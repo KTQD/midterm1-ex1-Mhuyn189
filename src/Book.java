f094498eedf71d506abd296f66e6f942df52e091
Mhuyn189 <157662286+Mhuyn189@users.noreply.github.com>
2024-03-14 07:35:02 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
-public class Book {
+public class Book extends Item{
+    String author;
+    int numberOfPages;
+    String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println("Author: " + author);
+        System.out.println("Number of pages: " + numberOfPages);
+        System.out.println("Genre: " + genre);
+    }
 }
