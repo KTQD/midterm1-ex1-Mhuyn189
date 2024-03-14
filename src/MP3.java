5542736a814e3cb10e853e03b0816ab0428d2b54
Mhuyn189 <157662286+Mhuyn189@users.noreply.github.com>
2024-03-14 07:34:05 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,14 @@
-public class MP3 {
+public class MP3 extends Item {
+  int duration;
+
+    public MP3(String name, String description, String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println("Duration: " + duration);
+    }
 }
