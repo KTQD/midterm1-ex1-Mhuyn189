8d66274699e10418c6306adf48051f0c2b3c015f
Mhuyn189 <157662286+Mhuyn189@users.noreply.github.com>
2024-03-14 07:31:42 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,27 @@
 public class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    float price;
+
+    public Item(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    void showInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + description);
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+    }
 }
