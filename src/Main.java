2c9d15ea98095e394e454e4847553bac3826d379
Mhuyn189 <157662286+Mhuyn189@users.noreply.github.com>
2024-03-14 07:36:41 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Ngay mai", "Bài hát", "NM1122", 12.3f,23);
+        System.out.println("Thông tin của MP3: ");
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Harry Potter","Tiểu thuyết", "1123", 34.5f,"J.K.Rowling", 200, "Detective");
+        System.out.println("Thông tin của sách: ");
         book.showInfo();
 
     }
